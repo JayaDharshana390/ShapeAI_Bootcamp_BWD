4021adcdda362649559927be8f6c1b521874b004
JayaDharshana390 <85471313+JayaDharshana390@users.noreply.github.com>
2021-06-07 09:45:06 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer(){
+  var Year= new Date().getFullYear();
+
+  return (
+             <footer>
+               <p>Copyright @ {Year}</p>
+             </footer>
+        
+  );
+}
+
+export default Footer;
